178ad45ea2448219712cd61f2c8992fd20d499e4
Toby Carlo <98287394+0xtobycarlo@users.noreply.github.com>
2022-03-16 15:54:52 +0000

    q1

---
+++ answers/q1.js
@@ -0,0 +1,13 @@
+function myFunction(num1, num2) {
+  return num1 + num2;
+}
+
+const result = myFunction(5, 5);
+
+// The function called 'myFunction' takes two arguments (datatype - number).
+
+// The block of code inside the curly braces is an expression that adds the two numbers together.
+
+// The 'return' keyword delivers a value of the expression and stops the function altogether, once the function is called.
+
+// The variable 'result' is holding the value returned from the function. When the function is logged in the console, the answer will show an answer of 10, as the argument called in the function equates to 5 + 5.
