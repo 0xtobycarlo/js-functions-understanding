589355da4fd8246a6f603cac58b394432e7bd96b
Toby Carlo <98287394+0xtobycarlo@users.noreply.github.com>
2022-03-16 16:29:36 +0000

    -.. --- -. . / . .-.. . ...- . -.

---
+++ answers/q11.js
@@ -0,0 +1,19 @@
+function myFunction(num1, num2, num3) {
+  return num1 + num2 + num3;
+}
+
+const num1 = 10;
+const num2 = 1;
+const num3 = 20;
+
+const result = myFunction(1, 1, 1);
+
+// The function takes three arguments. The function will add three numbers.
+
+// The function will return something since the return keyword is issued.
+
+// Variables have been declared and assigned to values.
+
+// Function is called with three numbers - not with the variables created - and then its value is stored in the var result.
+
+// I expect the variable result to hold a value of 3, since the code will first add the 2 numbers and then add to the 3rd number. =3.
