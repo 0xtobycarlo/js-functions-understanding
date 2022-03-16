06a0e11ac122959e7b5dcfcc5c207c8ebf8df914
Toby Carlo <98287394+0xtobycarlo@users.noreply.github.com>
2022-03-16 16:21:52 +0000

    n1n3

---
+++ answers/q9.js
@@ -0,0 +1,19 @@
+function myFunction(num1, num2) {
+  console.log(num3);
+}
+
+const num1 = 10;
+const num2 = 1;
+const num3 = 20;
+
+myFunction(num3, num1);
+
+// The function takes two arguments - num1 and num2.
+
+// It will display a value on the console.
+
+// Three variables (num1, num2, num3), have been declared with different numeric values.
+
+// The function is called with two arguments - num3 and num1.
+
+// I expect the console to display a value of 20, since the first argument called is num3, and so that's the first and only value that the console/function will accept.
