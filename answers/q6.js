dc2fc1794d660680e3b5b163d13aa7e7b26ce79d
Toby Carlo <98287394+0xtobycarlo@users.noreply.github.com>
2022-03-16 16:11:15 +0000

    6

---
+++ answers/q6.js
@@ -0,0 +1,20 @@
+function myFunction(num, num1) {
+  console.log(num1);
+}
+
+let num = 10;
+let num1 = 2;
+
+myFunction(num1, num);
+
+// The function takes two arguments (dataType = number).
+
+// The console.log will only display one argument.
+
+// Variable 'num' holds the value of 10.
+
+// Variable 'num1' holds the value of 2.
+
+// The function is called with two arguments.
+
+// I would presume the console will show a value of 2, and an error that reads" num is not defined.
