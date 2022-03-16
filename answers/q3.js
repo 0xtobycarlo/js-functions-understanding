bbf63ff8279f4a01b30da1a3d89e8c1fd5d6f8f5
Toby Carlo <98287394+0xtobycarlo@users.noreply.github.com>
2022-03-16 16:00:36 +0000

    3

---
+++ answers/q3.js
@@ -0,0 +1,17 @@
+function myFunction(num) {
+  return num - 1;
+}
+
+let num = 10;
+num = myFunction(num);
+num = myFunction(num);
+
+// This function takes a single argument (datatype = number).
+
+// It decrements a number by 1 each time.
+
+// The function returns a value - it has the `return` keyword.
+
+// The variable num holds the starting number/value of 10.
+
+// I expect the function to return the number/value of 8 as it has been called twice, due to the nature of the expression that the function is performing.
