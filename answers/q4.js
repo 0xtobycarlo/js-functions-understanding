0f72baa6da5ab0f45c6cacafd8b23bd4010f982f
Toby Carlo <98287394+0xtobycarlo@users.noreply.github.com>
2022-03-16 16:05:35 +0000

    4

---
+++ answers/q4.js
@@ -0,0 +1,22 @@
+function myFunction(num) {
+  return num - 1;
+}
+
+let num = 10;
+let add = 3;
+add = myFunction(add);
+add = myFunction(add);
+
+// This function takes one argument (datatype = number).
+
+// It decrements a number by 1.
+
+// It returns a value of the `return` keyword.
+
+// The variable 'num' holds a value of 10, and it is doing nothing.
+
+// The variable 'add' holds an integer value of 3 at the start.
+
+// The function is called twice - decrementing a number each time.
+
+// I expect the variable 'num' to still hold the value of number 10. I expect the variable 'add' to hold the value of 1, at this stage.
