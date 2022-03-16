025bd9d9d6f0430494d2b2ffe517ad1e132aa50f
Toby Carlo <98287394+0xtobycarlo@users.noreply.github.com>
2022-03-16 16:19:20 +0000

    81ght

---
+++ answers/q8.js
@@ -0,0 +1,21 @@
+function myFunction(num1, num2) {
+  return num1 + num2;
+}
+
+const num1 = 10;
+const num2 = 1;
+const num3 = 4;
+
+const result = myFunction(num3, num1);
+
+// Function takes two arguments - dataType and nums.
+
+// It adds the two numbers.
+
+// It returns a value because the return keyword is used.
+
+// Three variables have been initialised, each holding a different value of number.
+
+// The function is invoked with two arguments and the value of the function is stored in the 'result' variable.
+
+// I expect the result to be 4 + 10, which equates to 14.
