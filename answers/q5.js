eea933e4b37b78cdb4457f34c4d91f09591fcb11
Toby Carlo <98287394+0xtobycarlo@users.noreply.github.com>
2022-03-16 16:08:01 +0000

    Add files via upload

---
+++ answers/q5.js
@@ -0,0 +1,20 @@
+function myFunction(num, num1) {
+  console.log(num1);
+}
+
+let num = 10;
+let num1 = 2;
+
+myFunction(num);
+
+// The function takes two arguments (dataType = number).
+
+// The variable 'num' holds a value of 10.
+
+// The variable 'num1' holds a value of 2.
+
+// The function is called once with the variable 'num'.
+
+// I expect the function to return an error/undefined, as the 'num' variable is not defined inside the function.
+
+// num1 will not be displayed as it has not been called by the function.
